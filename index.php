5b1dd66d499f9778405d89f226700ee19e5f0d31
tural77 <turalinovruzov@gmail.com>
2020-08-02 18:21:51 +0400

    addForeachAssoc

--- index.php
+++ index.php
@@ -1,11 +1,26 @@
 <?php
 
+require "./DAL/CarRepository.php";
+
+
 // url
 if(isset($_GET["page"]) && $_GET["page"] != "index"){
     $page = $_GET["page"] . ".php";
 }else{
     $page ="main.php";
 }
+
+
+
+foreach(GetAllCars() as $x => $x_value) {
+    //echo "Key=" . $x . ", Value=" . $x_value;
+    //echo "<br>";
+    foreach($x_value as $inX => $inX_value) {
+        echo "Key=" . $inX . ", Value=" . $inX_value;
+        echo "<br>";
+    }
+    echo "<br> <br>";
+  }
 
 
 // file exists

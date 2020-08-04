c84701a7d47893ece87b75d7cfac0986696be3ef
tural77 <turalinovruzov@gmail.com>
2020-08-05 01:14:29 +0400

    newAdmin1

---
+++ DAL/AdminRepository.php
@@ -0,0 +1,138 @@
+<?php
+
+$CONN_STRING ="host='134.122.72.17' port='5432' dbname='phpstepproject' user='phpstepproject' password='phpstepproject'";
+$db_handle = pg_connect($CONN_STRING);
+
+
+function GetAllCars()
+{
+    global $db_handle;
+    
+    $pg_query = pg_query($db_handle, "
+select distinct Car.Id,Title,IsActive,added,updated, City.Name as City,Brand.Name as Brand,model.Name as Model,Year,Body_type.Name as BodyType,Color.Name as Color,EngineCapacity,HP,Fuel_type.Name as FuelType,Mileage,Gearbox_type.Name as GearboxType,Transmission.Name as Transmission,Price,Description, Car_Img.Img_Path from car
+    left join \"user\" u on Car.User_Id = u.Id
+    left join  city on Car.City_Id = city.Id
+    left join  model on Car.Model_Id = model.Id
+    left join  brand on Model.Brand_id = Brand.Id
+    left join  body_type  on Car.Body_type_Id = Body_type.id
+    left join  color  on Car.Color_Id = color.Id
+    left join  fuel_type  on Car.Fuel_type_Id =Fuel_type.Id
+    left join  gearbox_type  on Car.Gearbox_type_Id = Gearbox_type.Id
+    left join  transmission  on Car.Transmission_Id = transmission.Id
+    left join Car_Img on Car.Id = Car_Img.Car_Id
+    order by added desc;
+");
+
+    $arr = pg_fetch_all($pg_query, PGSQL_ASSOC);
+
+    //pg_close($db_handle);
+    return $arr;
+}
+
+
+function GetAllColors()
+{
+    global $db_handle;
+
+    $pg_query = pg_query($db_handle, "SELECT * FROM color;");
+
+    $arr = pg_fetch_all($pg_query, PGSQL_ASSOC);
+
+    //pg_close($db_handle);
+    return $arr;
+}
+
+
+
+function GetAllCities()
+{
+    global $db_handle;
+
+    $pg_query = pg_query($db_handle, "SELECT * FROM city;");
+
+    $arr = pg_fetch_all($pg_query, PGSQL_ASSOC);
+
+    //pg_close($db_handle);
+    return $arr;
+}
+
+
+function GetAllFuelTypes()
+{
+    global $db_handle;
+    
+    $pg_query = pg_query($db_handle, "SELECT * FROM fuel_type;");
+
+    $arr = pg_fetch_all($pg_query, PGSQL_ASSOC);
+
+    //pg_close($db_handle);
+    return $arr;
+}
+
+
+function GetAllGearboxTypes()
+{
+    global $db_handle;
+
+    $pg_query = pg_query($db_handle, "SELECT * FROM gearbox_type;");
+
+    $arr = pg_fetch_all($pg_query, PGSQL_ASSOC);
+
+    //pg_close($db_handle);
+    return $arr;
+}
+
+
+function GetAllTransmissions()
+{
+    global $db_handle;
+
+    $pg_query = pg_query($db_handle, "SELECT * FROM transmission;");
+
+    $arr = pg_fetch_all($pg_query, PGSQL_ASSOC);
+
+    //pg_close($db_handle);
+    return $arr;
+}
+
+
+function GetAllBodyTypes()
+{
+    global $db_handle;
+
+    $pg_query = pg_query($db_handle, "SELECT * FROM body_type;");
+
+    $arr = pg_fetch_all($pg_query, PGSQL_ASSOC);
+
+    //pg_close($db_handle);
+    return $arr;
+}
+
+
+function GetAllModels()
+{
+    global $db_handle;
+
+    $pg_query = pg_query($db_handle, "SELECT * FROM model;");
+
+    $arr = pg_fetch_all($pg_query, PGSQL_ASSOC);
+
+    //pg_close($db_handle);
+    return $arr;
+}
+
+
+function GetAllBrands()
+{
+    global $db_handle;
+   
+    $pg_query = pg_query($db_handle, "SELECT * FROM brand;");
+
+    $arr = pg_fetch_all($pg_query, PGSQL_ASSOC);
+
+    //pg_close($db_handle);
+    return $arr;
+}
+
+
+?>
--- admin.php
+++ admin.php
@@ -1,11 +1,26 @@
+<!DOCTYPE html>
+    <html lang="en">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.1/css/bootstrap.min.css" integrity="sha384-VCmXjywReHh4PwowAiWNagnWcLhlEJLA5buUprzK8rxFgeH0kww/aWY76TfkUoSX" crossorigin="anonymous">
+        <link rel="stylesheet" href="assets/css/vendor/font-awesome.css">
+        <title>Admin</title>
+    </head>
+
+    <body>
+
+    <header>
+    <h1 class="text-center display-1">Admin</h1>
+    </header>
+
+    
+
+
 <?php
 
-
-require "./DAL/BodyTypeRepository.php";
-require "./DAL/ModelRepository.php";
-require "./DAL/TransmissionRepository.php";
-require "./DAL/FuelTypeRepository.php";
-require "./DAL/GearboxTypeRepository.php";
+require "./DAL/AdminRepository.php";
 
 
 
@@ -20,16 +35,18 @@ ShowTables($getAllCars, "Cars");
 //ShowTables($getAllBrands, "Brands");
 //ShowTables($getAllCity, "Cities");
 
-
-
-
-
 function ShowTables($arrayAssoc, $tableName)
 {
 ?>
+
+
     <div class="m-3 p-1 shadow-sm border border-warning rounded">
 
-        <a class="display-4" id="testtt" data-toggle="collapse" href="#<?= $tableName ?>" aria-expanded="false" aria-controls="#<?= $tableName ?>"><?= $tableName ?></a>
+        <h2 class="mb-0">
+            <a style="text-decoration: none;" class="display-4 btn-block" data-toggle="collapse" href="#<?= $tableName ?>" ><?= $tableName ?></a>
+        </h2>
+
+        
 
         <div class="row">
 
@@ -41,7 +58,7 @@ function ShowTables($arrayAssoc, $tableName)
                             <table class="table table-striped table-sm  table-hover text-center">
 
                                 <thead class="thead-dark">
-                                    <tr >
+                                    <tr>
 
                                         <?php
 
@@ -71,11 +88,11 @@ function ShowTables($arrayAssoc, $tableName)
                                         }
 
                                         echo '<td>
-                                            <div class="btn-group btn-group-sm" role="group">
-                                                <a class="btn btn-info btn-sm" href="#"><i class="far fa-edit"></i></a>
-                                                <a class="btn btn-danger btn-sm" href="#"><i class="fas fa-trash"></i></a>
-                                            </div>
-                                        </td>';
+                                        <div class="btn-group btn-group-sm" role="group">
+                                            <a class="btn btn-info btn-sm" href="#"><i class="far fa-edit"></i></a>
+                                            <a class="btn btn-danger btn-sm" href="#"><i class="fas fa-trash"></i></a>
+                                        </div>
+                                    </td>';
 
 
                                         echo "</tr>";
@@ -93,41 +110,38 @@ function ShowTables($arrayAssoc, $tableName)
 
                                                 echo '<td>';
 
-                                                if (function_exists("GetAll" . $Key. "s") || function_exists("GetAll" . $Key)) {
+                                                if (function_exists("GetAll" . $Key . "s") || function_exists("GetAll" . $Key)) {
                                                     echo '<select class="form-control">
-                                                            <option selected disabled>' . ucfirst($Key) .'</option>';
-                                                            
-                                                            $TempAddFuncForAdmin = "GetAll" . $Key. "s";
+                                                        <option selected disabled>' . ucfirst($Key) . '</option>';
 
-                                                            foreach ($TempAddFuncForAdmin() as $index => $array) {
-                                                                foreach ($array as $Key => $Value) {
-                                                                    if(ucfirst($Key) == "Name")
-                                                                    echo '<option> '. $Value .'</option>';
-                                                                } 
-                                                            }
+                                                    $TempAddFuncForAdmin = "GetAll" . $Key . "s";
+
+                                                    foreach ($TempAddFuncForAdmin() as $index => $array) {
+                                                        foreach ($array as $Key => $Value) {
+                                                            if (ucfirst($Key) == "Name")
+                                                                echo '<option> ' . $Value . '</option>';
+                                                        }
+                                                    }
 
 
                                                     echo '</select>';
-
-                                                }elseif(ucfirst($Key) == "Id"){
-                                                    echo '<input readonly class="form-control" placeholder="' .ucfirst($Key) . '">';
-
-                                                }elseif(ucfirst($Key) == "Img_path"){
+                                                } elseif (ucfirst($Key) == "Id") {
+                                                    echo '<input readonly class="form-control" placeholder="' . ucfirst($Key) . '">';
+                                                } elseif (ucfirst($Key) == "Img_path") {
                                                     echo    '<div class="form-group"><label class="form-control" for="file-upload" style="border: 1px solid #ccc; display: inline-block; padding: 6px 12px; cursor: pointer;">
-                                                                <i class="fas fa-cloud-upload-alt"></i> Custom
-                                                            </label>
-                                                             <input  id="file-upload" style="display: none;" type="file" multiple/></div>';
-                                                }
-                                                 else {
-                                                    echo '<input class="form-control" placeholder="' .ucfirst($Key) . '">';
+                                                            <i class="fas fa-cloud-upload-alt"></i> Custom
+                                                        </label>
+                                                            <input  id="file-upload" style="display: none;" type="file" multiple/></div>';
+                                                } else {
+                                                    echo '<input class="form-control" placeholder="' . ucfirst($Key) . '">';
                                                 }
                                                 echo '</td>';
                                             }
 
                                             echo '<td>
-                                                        <a class="btn btn-primary btn-sm" style="width: 65px; height:37px;" href="#">
-                                                            <i class="fas fa-plus-circle fa-lg pt-2"></i></a>
-                                                </td>';
+                                                    <a class="btn btn-primary btn-sm" style="width: 65px; height:37px;" href="#">
+                                                        <i class="fas fa-plus-circle fa-lg pt-2"></i></a>
+                                            </td>';
 
 
                                             echo "</tr>";
@@ -149,6 +163,16 @@ function ShowTables($arrayAssoc, $tableName)
         </div>
     </div>
 
+
+
+
 <?php
 }
+pg_close($db_handle);
 ?>
+
+
+        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.1/js/bootstrap.min.js" integrity="sha384-XEerZL0cuoUbHE4nZReLT7nx9gQrQreJekYhJD9WNWhH8nEW+0c5qq7aIo2Wl30J" crossorigin="anonymous"></script>
+    </body>
+</html>

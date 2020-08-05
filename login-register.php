c1d88ea50cbb781552ef91993329dc90a04eb15b
mustafayevt <mustafayevtural46@gmail.com>
2020-08-05 18:40:59 +0400

    login-register view issue fixed

--- login-register.php
+++ login-register.php
@@ -54,39 +54,39 @@ $error_msg = '';
             </div>
             <input type="hidden" name="login-register" value="login">
             </form>
-        </div>
-        <div class="col-sm-12 col-md-12 col-lg-6 col-xs-12">
-            <form action="?page=login-register" method="post">
-                <div class="login-form">
-                    <h4 class="login-title">Register</h4>
-                    <div class="row">
-                        <div class="col-md-6 col-12 mb--20">
-                            <label>Name</label>
-                            <input name="name" type="text" placeholder="First Name">
-                        </div>
-                        <div class="col-md-6 col-12 mb--20">
-                            <label>Surname</label>
-                            <input name="surname" type="text" placeholder="Last Name">
-                        </div>
-                        <div class="col-md-12">
-                            <label>Email Address*</label>
-                            <input name="email" type="email" placeholder="Email Address">
-                        </div>
-                        <div class="col-md-6">
-                            <label>Password</label>
-                            <input name="password" type="password" placeholder="Password">
-                        </div>
-                        <div class="col-md-6">
-                            <label>Confirm Password</label>
-                            <input name="conf_password" type="password" placeholder="Confirm Password">
-                        </div>
-                        <div class="col-12">
-                            <button class="uren-register_btn">Register</button>
+            <div class="col-sm-12 col-md-12 col-lg-6 col-xs-12">
+                <form action="?page=login-register" method="post">
+                    <div class="login-form">
+                        <h4 class="login-title">Register</h4>
+                        <div class="row">
+                            <div class="col-md-6 col-12 mb--20">
+                                <label>Name</label>
+                                <input name="name" type="text" placeholder="First Name">
+                            </div>
+                            <div class="col-md-6 col-12 mb--20">
+                                <label>Surname</label>
+                                <input name="surname" type="text" placeholder="Last Name">
+                            </div>
+                            <div class="col-md-12">
+                                <label>Email Address*</label>
+                                <input name="email" type="email" placeholder="Email Address">
+                            </div>
+                            <div class="col-md-6">
+                                <label>Password</label>
+                                <input name="password" type="password" placeholder="Password">
+                            </div>
+                            <div class="col-md-6">
+                                <label>Confirm Password</label>
+                                <input name="conf_password" type="password" placeholder="Confirm Password">
+                            </div>
+                            <div class="col-12">
+                                <button class="uren-register_btn">Register</button>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <input type="hidden" name="login-register" value="register">
-            </form>
+                    <input type="hidden" name="login-register" value="register">
+                </form>
+            </div>
         </div>
     </div>
 </div>

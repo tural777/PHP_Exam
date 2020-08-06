1f888601c61225e46e01f144df027fd3e498c2f3
tural77 <turalinovruzov@gmail.com>
2020-08-06 12:46:23 +0400

    addAdminRepo

--- DAL/AdminRepository.php
+++ DAL/AdminRepository.php
@@ -1,14 +1,24 @@
 <?php
 
+function ShowAllTables()
+{
+    global $db_handle;
 
+    $pg_query = pg_query($db_handle, "SELECT table_name
+    FROM information_schema.tables
+    WHERE table_schema='public'
+     AND table_type='BASE TABLE'");
+     
+    return pg_fetch_all($pg_query, PGSQL_ASSOC);
+}
 function GetAllCars()
 {
     global $db_handle;
     
     $pg_query = pg_query($db_handle, "
-select distinct Car.Id,Title,IsActive,added,updated, City.Name as Cities,Brand.Name as Brand,model.Name as Model,
-Year,Body_type.Name as BodyType,Color.Name as Color,EngineCapacity,HP,Fuel_type.Name as FuelType,Mileage,
-Gearbox_type.Name as GearboxType,Transmission.Name as Transmission,Price,Description from car
+select  Car.Id,Title,IsActive,City.Name as Cities,Brand.Name as Brands,model.Name as Models,
+Year,Body_type.Name as BodyTypes,Color.Name as Colors,EngineCapacity,HP,Fuel_type.Name as FuelTypes,Mileage,
+Gearbox_type.Name as GearboxTypes,Transmission.Name as Transmissions,Price,Description from car
     left join \"user\" u on Car.User_Id = u.Id
     left join  city on Car.City_Id = city.Id
     left join  model on Car.Model_Id = model.Id
@@ -116,7 +126,9 @@ function GetAllUsers()
 {
     global $db_handle;
 
-    $pg_query = pg_query($db_handle, "SELECT * FROM \"user\";");
+    $pg_query = pg_query($db_handle, 
+    'select "user".Id,"user".name,surname,Email,Password,Role.Name as Roles from "user"
+    left join Role on Role_Id = Role.Id');
 
     return pg_fetch_all($pg_query, PGSQL_ASSOC);
 }
@@ -155,6 +167,36 @@ function InsertModel($name, $brandId)
     
     pg_query($db_handle, $query);
 }
+
+
+
+function InsertUser($name, $surname, $email, $password, $role_id)
+{
+    global $db_handle;
+    $query = "insert into \"user\" (email, password,role_id,name,surname) 
+    values ('$email','$password','$role_id','$name','$surname')";
+    $pg_query = pg_query($db_handle, $query);
+    //$result = pg_fetch_row($pg_query);
+    //return $result[0];
+}
+
+
+function InsertCar($user_id, $title ,$city_id, $model_id, $year, $body_type_id, $color_id, $engineCapacity, $HP, $fuel_type_id, $mileage, $gearbox_type_id, $transmission_id, $price, $description, $Images = null, $isActive = 0)
+{
+    global $db_handle;
+    $query = "insert into car (User_Id,Title, IsActive,City_Id,Model_Id, Year,Body_type_Id,Color_Id, EngineCapacity, HP,Fuel_type_Id, Mileage,Gearbox_type_Id,transmission_id, Price, Description)
+    values ('$user_id','$title','$isActive','$city_id','$model_id','$year','$body_type_id','$color_id','$engineCapacity','$HP','$fuel_type_id','$mileage','$gearbox_type_id','$transmission_id','$price','$description') RETURNING id;";
+    pg_query($db_handle, $query);
+    
+}
+
+
+function GenericDeleteById($table, $Id){
+    global $db_handle;
+    $query = "delete from \"$table\" where id = '$Id'";
+    pg_query($db_handle, $query);
+}
+
 
 
 ?>
